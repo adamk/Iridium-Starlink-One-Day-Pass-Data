d0c07de1d07f2b0b56e726c2dc3dc980667395ba
Adam Kimbrough <adamk@users.noreply.github.com>
2021-09-26 19:34:56 -0400

    Updated and fixed bugs
    Fixed a bug with incorrect use of replace(). Used re.sub() instead.

--- get_passes.py
+++ get_passes.py
@@ -6,7 +6,7 @@
 # period to file 'local_passes.dat'
 
 # Author: Adam Kimbrough
-# September 08, 2021
+# September 21, 2021
 # HOW TO RUN: Place file inside /predict/default/ folder (with predict.qth) and run "python3 get_passes.py"
 # Dependencies: skyfield API, pandas
 
@@ -20,6 +20,7 @@ from skyfield.api import load, wgs84
 from itertools import islice
 from datetime import datetime
 
+# Pull latest Iridium-NEXT TLE Data
 if os.path.exists('iridium.tle'):
     os.remove('iridium.tle')
 stations_url = 'http://celestrak.com/NORAD/elements/iridium-NEXT.txt'
@@ -31,6 +32,7 @@ iridium_sats = list(by_name.keys())
 
 count = 0
 
+# Separate TLE data into multiple files for PREDICT to process w/o error (maximum TLE line limit)
 with open('iridium.tle', 'r') as infile:
     with open('iridium1.tle', 'w') as outfile:
         for TLE in list(islice(infile, 72)):
@@ -45,25 +47,20 @@ with open('iridium.tle', 'r') as infile:
         for TLE in list(islice(infile, 72)):
             outfile.write(TLE)         
         
-        
+# User input for specific day of predictions       
 date_range = input("Enter single day as Jan 01 2021 \n")
 cmdln_start = 'date -d "' + date_range + ' 04:00:00 UTC" ' + '+%s'
 start = os.popen(cmdln_start).read().strip()
 print(start)
 date_range_end = int(date_range[4:6])+1
-new_date_range = date_range.replace(date_range[4:6], str(date_range_end))
+new_date_range = re.sub("(\w{3} )\d{2}( \d{4})", r"\g<1>%02d\g<2>" % date_range_end, date_range)
+print(new_date_range)
 cmdln_end = 'date -d "' + str(new_date_range) + ' 03:59:59 UTC" ' + '+%s'
 end = os.popen(cmdln_end).read().strip()
 print(end)
-time_list = []
-x = 0
-y = 0
-while x < 18:
-    time_list.append(int(start) + 5000*y)
-    x+=1
-    y+=1
-    k = 1
+k = 1
 sat_count = 0
+# Run PREDICT on all Iridium-NEXT satellites given the date input
 while k <= 3:
     count = 0
     while count < 24:
@@ -83,7 +80,7 @@ while count4 <= 3:
     os.system(cmdln_predict)
     count4 += 1
     sat_count += 1
-
+# Write pass data into 'passes.dat'
 sat_count3 = 0
 with open('passes.dat', 'w') as outfile:
     while sat_count3 < len(iridium_sats):
@@ -91,7 +88,6 @@ with open('passes.dat', 'w') as outfile:
         sat_count2 = 0
         sat_name = iridium_sats[sat_count3].replace(" ", "") + '.dat'
         with open(sat_name.replace(" ", "")) as infile:
-            
             for line in infile:
                 if line.split(' ')[4] == '':
                     if line.split(' ')[5] == '':
@@ -106,6 +102,7 @@ with open('passes.dat', 'w') as outfile:
         os.remove(sat_name)
         sat_count3 += 1
 
+# Remove the duplicate timestamps
 n=0
 output_file = "new_passes.dat"
 new_lines = []
@@ -135,6 +132,7 @@ with open('passes.dat', 'r') as infile, open(output_file, 'w') as outfile:
            new_lines.append(time)    
     print(iridium_sats[n-1] + ' duplicates removed.')
 
+# Convert the UTC time to Local (ET) and write to 'local_passes.dat'
 n=0
 local_day = date_range[4:6]
 print('Converting UTC to Local...\n')
@@ -155,18 +153,17 @@ with open('new_passes.dat', 'r') as infile, open('local_passes.dat', 'w') as out
                     local_hour = str(0) + str(local_hour)
                 else:
                     local_hour = int(utc[0:2])-4
-                local_time = utc.replace(utc[0:2], str(local_hour))
-                utc_day = line.split(' ')[2]
-                local_line = re.sub(utc[0:8],local_time, line)
-                local_line = re.sub("\d+(?=\string)", local_day, line)
+                new_line = re.sub("\d{2}(\:\d{2}:\d{2})", r"%02d\1" % int(local_hour), line)
+                local_line = re.sub("\d+(?=[A-Z])", local_day, new_line)
                 outfile.write(local_line) 
-    print('\nDone.')
+    print('Done.')
       
     
 n = 0
 output_file2 = 'num_passes.dat'
 dates = []
 
+# Sort the times from 00:00:00 to 23:59:59 and write the # of passes at each timestamp to 'num_passes.dat'
 print("\nSorting times...")
 with open('local_passes.dat', 'r') as infile, open(output_file2, 'w') as outfile:
     for line in infile:
@@ -178,4 +175,3 @@ with open('local_passes.dat', 'r') as infile, open(output_file2, 'w') as outfile
         time = key[:2] + ':' + key[2:4] + ':' + key[4:]
         outfile.write(time +'\t'+ '# of sats above horizon: ' + str(time_dict[key]) + '\n')
     print("Number of sats at each timestamp written to file.")
- 
